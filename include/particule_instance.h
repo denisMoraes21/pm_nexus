653b5ad6968099b6bb67def46a20217c6e41fdaf
israel malheiros <israelmalheirosoliveira@gmail.com>
2026-04-16 20:38:08 -0400

    adição de pacote

--- include/particule_instance.h
+++
@@ -1,5 +0,0 @@
-#include "DFRobot_AirQualitySensor.h"
-
-#define I2C_ADDRESS    0x19
-
-DFRobot_AirQualitySensor particle(&Wire ,I2C_ADDRESS);
---
+++ include/temperature_definition.h
@@ -0,0 +1,5 @@
+#include <Adafruit_BME280.h>
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME280 bme;
+unsigned long delayTime;
